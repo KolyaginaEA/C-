d514171f72132cf2761019a8f17846cd51f940d3
Ekaterina <katya_usova@bk.ru>
2023-01-18 23:28:23 +0300

    Задача 8

---
+++ HomeWork008/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count<=numberN)
+    {
+        Console.Write(count + " ");
+        count = count + 2;
+    }
